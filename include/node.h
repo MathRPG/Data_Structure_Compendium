4003f0fbb411eaf463e24d5699a2448c73bae960
MathRPG <maugusilva.santos@gmail.com>
2021-03-04 22:39:46 -0300

    Refactor

--- include/node.h
+++ include/node.h
@@ -5,9 +5,10 @@
 
 typedef struct Node_s Node_t;
 
-void node_delete(Node_t* pS);
-Node_t* node_new(const void* data, size_t size);
-const void* node_data(const Node_t* node);
-Node_t** node_next(Node_t* node);
+Node_t* node_new(const void* data, size_t data_size);
+void node_delete(Node_t*);
+
+const void* node_data(const Node_t*);
+Node_t** node_next(Node_t*);
 
 #endif //DATA_STRUCTURES_NODE_H
--- src/node.c
+++ src/node.c
@@ -9,26 +9,31 @@ struct Node_s
 	Node_t* next;
 };
 
-void node_delete(Node_t* pS)
-{
-	free(pS->data);
-	free(pS);
-}
-
-Node_t* node_new(const void* data, size_t size)
+Node_t* node_new(const void* data, const size_t data_size)
 {
 	Node_t* new_node = malloc(sizeof(*new_node));
-	new_node->data = malloc(size);
-	memcpy(new_node->data, data, size);
+
+	*new_node = (struct Node_s){
+			.data = malloc(data_size),
+			.next = NULL,
+	};
+
+	memcpy(new_node->data, data, data_size);
 	return new_node;
 }
 
-const void* node_data(const Node_t* node)
+void node_delete(Node_t* const node)
+{
+	free(node->data);
+	free(node);
+}
+
+const void* node_data(const Node_t* const node)
 {
 	return node->data;
 }
 
-Node_t** node_next(Node_t* node)
+Node_t** node_next(Node_t* const node)
 {
 	return &node->next;
 }
